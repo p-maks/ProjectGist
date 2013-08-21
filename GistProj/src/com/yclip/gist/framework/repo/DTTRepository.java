2d18434f42c8bc33ec63a6cbe0324a4de1ca03be
Chaka <Chaka@161.76.253.119>
2013-08-21 14:32:40 +0100

    Added repo package and DTTRepository class

---
+++ GistProj/src/com/yclip/gist/framework/repo/DTTRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yclip.gist.framework.repo;
+
+/**
+ *
+ * @author Chaka
+ */
+public class DTTRepository {
+    
+    
+    
+    public boolean contains(String word){
+        return false;
+    }
+    
+}
